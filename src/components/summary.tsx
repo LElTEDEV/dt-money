0c9ffa2751095d8db8276ea2fd8aca3a0912ba09
LElTEDEV <zinenizi@gmail.com>
2025-12-15 11:36:39 -0300

    feat: criado summary component

---
+++ src/components/summary.tsx
@@ -0,0 +1,24 @@
+import { CircleArrowDown, CircleArrowUp, DollarSign } from "lucide-react";
+import { SummaryCard } from "./summaryCard";
+
+export function Summary() {
+  return (
+    <section className="w-full max-w-280 mx-auto px-6 grid grid-cols-3 gap-8 -mt-20">
+      <SummaryCard
+        title="Entradas"
+        amount="17.400,00"
+        icon={CircleArrowUp}
+        iconColor="green"
+      />
+
+      <SummaryCard
+        title="Saídas"
+        amount="17.400,00"
+        icon={CircleArrowDown}
+        iconColor="red"
+      />
+
+      <SummaryCard title="Total" amount="17.400,00" icon={DollarSign} />
+    </section>
+  );
+}
