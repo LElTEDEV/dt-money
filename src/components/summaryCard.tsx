ad00aa859d3b773273e957ede79644c2c94f4a5d
LElTEDEV <zinenizi@gmail.com>
2025-12-15 11:36:21 -0300

    feat: criado summaryCard component

---
+++ src/components/summaryCard.tsx
@@ -0,0 +1,35 @@
+import { type LucideIcon } from "lucide-react";
+
+type SummaryIconProps = "green" | "red" | "default";
+
+type SummaryCardProps = {
+  title: string;
+  icon: LucideIcon;
+  amount: string;
+  iconColor?: SummaryIconProps;
+};
+
+const IconColor = {
+  green: "#00b37e",
+  red: "#f75a68",
+  default: "#fff",
+};
+
+export function SummaryCard({
+  title,
+  amount,
+  icon: Icon,
+  iconColor = "default",
+}: SummaryCardProps) {
+  return (
+    <div className="bg-gray-700 rounded-lg p-8">
+      <header className="flex items-center justify-between text-gray-300">
+        <span>{title}</span>
+
+        <Icon size={25} color={IconColor[iconColor]} />
+      </header>
+
+      <strong className="block mt-4 text-[2rem]">R$ {amount}</strong>
+    </div>
+  );
+}
